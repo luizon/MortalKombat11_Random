9482b1d4381c9a169a542d8fdc1cbd4c51548cbb
Luis Fernando Cárdenas Vega <50283816+luizon@users.noreply.github.com>
2020-09-26 00:07:25 -0600

    First try
    Later I'll put images and animations, maybe, idk.

---
+++ mk11_random.js
@@ -0,0 +1,51 @@
+var character = ["ROBOCOP",
+                "SHEEVA",
+                "FUJIN",
+                "SPAWN",
+                "THE JOKER",
+                "JADE",
+                "ERRON BLACK",
+                "KABAL",
+                "KUNG LAO",
+                "SUB-ZERO",
+                "SCORPION",
+                "CENTRION",
+                "FROST",
+                "BARAKA",
+                "RAIDEN",
+                "SHAO KAHN",
+                "D'VORAH",
+                "JAX BRIGGS",
+                "GERAS",
+                "KANO",
+                "TERMINATOR T-800",
+                "NIGHTWOLF",
+                "CASSIE CAGE",
+                "KOTAL KAHN",
+                "SKARLET",
+                "SONYA BLADE",
+                "SHANG TSUNG",
+                "JOHNNY CAGE",
+                "NOOB SAIBOT",
+                "KOLLECTOR",
+                "KITANA",
+                "JACQUI BRIGGS",
+                "LIU KANG",
+                "SINDEL"];
+var h1_character;
+var btn;
+
+var randomize = () => {
+    let index = Math.floor(Math.random()*character.length);
+    h1_character.innerHTML = character[index];
+};
+
+;(function() {
+    h1_character = document.createElement("h1");
+    h1_character.innerHTML = "<i>Press the button</i>";
+    btn = document.createElement("button");
+    btn.onclick = randomize;
+
+    document.body.append(h1_character);
+    document.body.append(btn);
+})();
